b6021e5f3344e9b4ab212511a157ff7a97a19f2e
maaaru09 <h.akimaru08@gmail.com>
2023-11-04 12:38:15 +0900

    ご指摘頂いた箇所の修正を行いました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,38 +3,33 @@
 require "date"
 require 'optparse'
 
-# オプションの設定
 opt = OptionParser.new
-opt.on('-m')
-opt.on('-y')
+opt.on('-m') {|v| v}
+opt.on('-y') {|v| v}
 opt.parse!(ARGV)
 
-mon_option = ARGV[0].to_i
-year_option = ARGV[1].to_i
+month = ARGV[0].to_i
+year = ARGV[1].to_i
 
-# デフォルトの設定
 today = Date.today
-if mon_option == 0
-  mon_option = today.mon
+if month == 0
+  month = today.month
 end
 
-if year_option == 0
-  year_option = today.year
+if year == 0
+  year = today.year
 end
 
-# カレンダーを表示するコード
-day1 = Date.new(year_option, mon_option, 1)
-day31 = Date.new(year_option, mon_option, -1)
-days = [*day1..day31]
-youbi = ["日", "月", "火", "水", "木", "金", "土"]
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
+days = [*first_day..last_day]
 
-puts "#{mon_option}月 #{year_option}".center(20)
-puts youbi.join(' ')
-print "   " * day1.wday
+puts "#{month}月 #{year}".center(20)
+puts "日 月 火 水 木 金 土"
+print "   " * first_day.wday
 days.each do |day|
+  print day.strftime('%-d').center(3)
   if day.saturday?
-    print "#{day.strftime('%-d')}\n".to_s.rjust(3)
-  else
-    print "#{day.strftime('%-d')}".to_s.center(3)
+    print "\n"
   end
 end
