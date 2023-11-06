a310eda48aeff32b673a1292632bb05ad446ac2b
maaaru09 <h.akimaru08@gmail.com>
2023-11-06 11:49:14 +0900

    ご指摘頂いた箇所の修正を行いました。3回目の再提出です。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,17 +8,9 @@ opt.on('-m [month]', Integer) {|v| options[:m] = v}
 opt.on('-y [year]', Integer) {|v| options[:y] = v}
 opt.parse(ARGV)
 
-month = options[:m]
-year = options[:y]
-
 today = Date.today
-if month == nil
-  month = today.month
-end
-
-if year == nil
-  year = today.year
-end
+month = options[:m] || today.month
+year = options[:y] || today.year
 
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
@@ -28,11 +20,10 @@ puts "日 月 火 水 木 金 土"
 print "   " * first_date.wday
 
 first_date.upto(last_date) do |date|
-  print date.strftime('%-d').rjust(2)
+  print date.day.to_s.rjust(2)
   if date.saturday?
     print "\n"
-  end
-  unless date.saturday?
+  else
     print " "
   end
 end
