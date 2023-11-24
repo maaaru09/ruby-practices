163333834744e2d1bf34990cfcc9698f70b919ab
maaaru09 <h.akimaru08@gmail.com>
2023-11-24 16:50:35 +0900

    lsコマンド1の提出物です

---
+++ 04.ls/ls.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+def main
+  files = Dir.glob('*')
+  columns = slice_files(files, 3)
+  display_columns(columns)
+end
+
+def slice_files(files, number)
+  files << ' ' until (files.size % number).zero?
+  files.each_slice(files.size / number).to_a
+end
+
+def display_columns(columns)
+  column_width = columns.flatten.max_by(&:length).size
+  columns.transpose.each do |column|
+    column.each do |file|
+      print file.ljust(column_width + 1)
+    end
+    puts "\n"
+  end
+end
+
+main
